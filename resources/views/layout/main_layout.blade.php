cfcb419ec6ad5b3b1317ba0716121a36dd105af3
professoreugenio <professoreugeniomls@gmail.com>
2024-12-11 00:32:46 -0300

    001-lofinform

---
+++ resources/views/layout/main_layout.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Página Notes</title>
+    <link rel="stylesheet" href="{{ asset('assets/bootstrap/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('assets/fontawesome/css/all.min.css')}}">
+    <link rel="shorcut icon" href="{{ asset('assets/images/favicon.png')}}">
+</head>
+<body>
+    @yield('content')
+    <script src="{{ asset('assets/bootstrap/bootstrap.bundle.min.js')}}"></script>
+</body>
+</html>
